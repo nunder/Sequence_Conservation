4cec04f3f418029cea4a82ebe2ecfcedb8170cff
nunder <90188497+nunder@users.noreply.github.com>
2022-02-27 16:40:52 +0000

    Updated 2022-02-27 16:40

--- .ipynb_checkpoints/Sequence_Analysis_Routines-checkpoint.py
+++ .ipynb_checkpoints/Sequence_Analysis_Routines-checkpoint.py
@@ -73,29 +73,52 @@ def generate_protein_file(input_fileloc, output_fileloc):
 def run_sonic_paranoid(protein_file_location, output_location, run_name):
     subprocess.run('wsl cd ~; source sonicparanoid/bin/activate; sonicparanoid -i ' + wslname(protein_file_location) +' -o ' + 
     wslname(output_location) + ' -p ' + run_name + ' -t 8' , shell=True)
-    
-    
-#  Orthologue class
 
-class Ortholog_Grouping:
-    def __init__(self, file_loc):
-        orthologs_df = pd.read_csv(file_loc, sep="\t", header=0)
-        file_list = list(orthologs_df)
-        file_list.remove('group_id')
-        self.num_ids = len(file_list)
-        orthologs_df = orthologs_df.melt(id_vars = ['group_id'], value_vars = file_list)
-        orthologs_df = orthologs_df.rename(columns={'variable':'species', 'value':'protein_id'})
-        for i, r in tqdm(orthologs_df.iterrows(), total=orthologs_df.shape[0]):
-            orthologs_df.at[i,'species'] = r['species'][:-4]
-            if r['protein_id'][0] == '*':
-                orthologs_df.at[i,'num_protein_ids'] = 0
+def read_fasta_to_arrays(filename):
+    with open(filename,'r') as ofile: 
+            sequence_names = []
+            sequence_list = []
+            first_seq = 0
+            for l in ofile:
+                m = l.strip('\n')
+                if m[0] == '>':
+                    if first_seq > 0:
+                        sequence_list.append(outstr)
+                    outstr = ''
+                    sequence_names.append(m[1:])
+                else:
+                    first_seq = 1
+                    outstr += m
+            sequence_list.append(outstr)
+    num_sequences = len(sequence_names) 
+    return [sequence_names, sequence_list]
+    
+def align_and_build(id_list, num_subsets, subset_num, source_data, length_field, seq_field, out_loc, min_species): 
+    muscle_exe = 'C:/Users/nicho/Muscle/muscle3.8.31_i86win32.exe'
+    ids = chunk_list(id_list, num_subsets, subset_num)
+    for j in ids:
+        temp_df = source_data[source_data['group_id'] == j]
+        num_non_zero = 0
+        num_zero = 0
+        for i, r in temp_df.iterrows():
+            if (r[length_field] > 0):
+                num_non_zero += 1
             else:
-                orthologs_df.at[i,'protein_id'] = r['protein_id'].split(',')[0]
-                orthologs_df.at[i,'num_protein_ids'] = len(r['protein_id'].split(','))
-        self.orthologs_df = orthologs_df[orthologs_df['num_protein_ids']==1]   #  Remove species not in ortholog group or where more than one protein 
-        temp_df = self.orthologs_df.groupby('group_id',as_index=False)['protein_id'].count()
-        self.single_copy_ortholog_groups = temp_df['group_id'].tolist()
-        self.full_ortholog_groups = temp_df[temp_df['protein_id'] == self.num_ids]['group_id'].tolist()
+                num_zero += 1
+        if ((num_non_zero >= min_species)):
+            ofile = open(out_loc+'temp'+str(j)+'.fasta', "w")
+            for i, r in temp_df.iterrows():
+                if (r[length_field] > 0):
+                    ofile.write(">" + r['species'] + "\n" + r[seq_field] + "\n")
+                else:
+                    ofile.write(">" + r['species'] + "\n" + "\n")
+            ofile.close() 
+            cline = MuscleCommandline(muscle_exe, input=out_loc +'temp'+str(j)+'.fasta', out=out_loc+str(j)+'.fasta')
+            try:
+                stdout, stderr = cline()
+            except Exception as e:
+                pass
+            delete_if_exists(out_loc +'temp'+str(j)+'.fasta')
 
 def parse_genbank(input_filename, non_cds_offset = 0):
     offset = non_cds_offset
@@ -206,7 +229,170 @@ def parse_genbank(input_filename, non_cds_offset = 0):
                 df.at[i,'ss_non_cds_loc'] = '' 
                 df.at[i,'ss_non_cds_length'] = 0
                    
-    df = df[['name','type','locus_tag','previous_locus_tag','next_locus_tag','protein_id','bp_restrict','loc','non_cds_loc','upstream_non_cds_loc','ss_non_cds_loc','strand','start','end', 'cds_seq','non_cds_seq',
+    df = df[['name','type','locus_tag','previous_locus_tag','next_locus_tag','protein_id','bp_restrict','loc','non_cds_loc','upstream_non_cds_loc','ss_non_cds_loc','strand','prev_strand','next_strand','start','end', 'cds_seq','non_cds_seq',
               'non_cds_offset_seq', 'upstream_non_cds_seq', 'non_cds_start','non_cds_end','upstream_non_cds_start','upstream_non_cds_end','ss_non_cds_seq','cds_length','non_cds_length','upstream_non_cds_length','ss_non_cds_length',
               'non_cds_offset_length','non_cds_offset_start','non_cds_offset_stop']]                                  
     return df
+
+def relative_entropy(sequence_list, alphabet_name = 'NT', *args, **kwargs):
+    alphabet_list = kwargs.get('alphabet_list',[])
+    background_probabilities =kwargs.get('background_probabilities',[]) 
+    element_wise = kwargs.get('element_wise',True)
+    insertions_as_background = kwargs.get('insertions_as_background',True)
+    exclude_insertions = kwargs.get('exclude_insertions',False)
+    insertion_character = kwargs.get('insertion_character','-')
+    if alphabet_name == 'AA':
+        alphabet_list = ['D', 'I', 'A', 'S', 'P', 'Y', 'V', 'Q', 'T', '*', 'H', 'G', 'R', 'F', 'W', 'N', 'C', 'K', 'L', 'M', 'E']
+    elif alphabet_name == 'NT':
+        alphabet_list =['A','C','T','G']
+    if insertions_as_background == False:
+        alphabet_list.append(insertion_character)
+    if len(background_probabilities) > 0:
+        background_probs = background_probabilities
+    else:
+        background_probs = [1/len(alphabet_list)]*len(alphabet_list)
+    num_symbols = len(alphabet_list)
+    num_sequences = len(sequence_list)
+    sequence_length = len(sequence_list[0])
+    relent_list = []
+    cumulative_relent = 0
+    for i in range(sequence_length):
+        relent = 0
+        vals = [v[i] for v in sequence_list]
+        if (exclude_insertions == False) or (not(insertion_character in vals)):
+            for j in range(num_symbols):
+                if insertions_as_background == True:
+                    ct = vals.count(alphabet_list[j]) + vals.count(insertion_character) * background_probs[j]
+                else:
+                    ct = vals.count(alphabet_list[j]) 
+                if ct == 0:
+                    pass
+                else:
+                    relent = relent + (ct/num_sequences) * math.log((ct/num_sequences)/background_probs[j],2)
+            cumulative_relent = cumulative_relent + relent  
+            relent_list.append(relent)
+    if element_wise == True:
+        return relent_list
+    else:
+        return cumulative_relent
+#  Orthologue class
+
+class Ortholog_Grouping:
+    def __init__(self, file_loc):
+        orthologs_df = pd.read_csv(file_loc, sep="\t", header=0)
+        file_list = list(orthologs_df)
+        file_list.remove('group_id')
+        self.num_ids = len(file_list)
+        orthologs_df = orthologs_df.melt(id_vars = ['group_id'], value_vars = file_list)
+        orthologs_df = orthologs_df.rename(columns={'variable':'species', 'value':'protein_id'})
+        for i, r in tqdm(orthologs_df.iterrows(), total=orthologs_df.shape[0]):
+            orthologs_df.at[i,'species'] = r['species'][:-4]
+            if r['protein_id'][0] == '*':
+                orthologs_df.at[i,'num_protein_ids'] = 0
+            else:
+                orthologs_df.at[i,'protein_id'] = r['protein_id'].split(',')[0]
+                orthologs_df.at[i,'num_protein_ids'] = len(r['protein_id'].split(','))
+        self.orthologs_df = orthologs_df[orthologs_df['num_protein_ids']==1]   #  Remove species not in ortholog group or where more than one protein 
+        temp_df = self.orthologs_df.groupby('group_id',as_index=False)['protein_id'].count()
+        self.single_copy_ortholog_groups = temp_df['group_id'].tolist()
+        self.full_ortholog_groups = temp_df[temp_df['protein_id'] == self.num_ids]['group_id'].tolist()
+
+
+
+class Ortholog_Sequence_Dataset:
+    def __init__(self, ortholog_grouping, genome_datasets_dir, genome_ids, offset, master_species):
+        df_list = list()
+        match_stats = list()
+        for id in tqdm(genome_ids):
+            cds_data = parse_genbank(genome_datasets_dir + '/' + id +'/genomic.gbff',offset)
+            orthologs_for_id = ortholog_grouping.orthologs_df[ortholog_grouping.orthologs_df['species'] == id]
+            orthologs_and_cds_info = orthologs_for_id.merge(cds_data, how = 'left', left_on = 'protein_id', right_on = 'protein_id')
+            orthologs_and_cds_info.drop_duplicates(subset = ['protein_id'], keep=False, inplace=True)   # Remove instances where same protein is coded in multiple locuses
+            temp_1 = len(orthologs_and_cds_info[orthologs_and_cds_info['name']==orthologs_and_cds_info['name']])
+            temp_2 = orthologs_and_cds_info.protein_id.nunique()
+            match_stats.append([id, temp_1, temp_2, round(temp_1/temp_2*100,1)]) 
+            orthologs_and_cds_info = orthologs_and_cds_info[orthologs_and_cds_info['name']==orthologs_and_cds_info['name']]    #Remove proteins which can't be matched back
+            df_list.append(orthologs_and_cds_info)
+        self.sequence_data = pd.concat(df_list)  
+        self.match_info = pd.DataFrame(match_stats, columns = ['id','num_orthologs','num_matches','match_pct'])
+        self.master_species = master_species
+        #dna_data.to_csv(output_dir + '/Ortholog_Master_Data/'+'dna_data.csv')
+        #match_info.to_csv(output_dir + '/Ortholog_Master_Data/'+'match_info.csv')
+    
+    def master_species_info(self, group_id, fieldname):
+        temp_df = self.sequence_data[self.sequence_data['group_id'] == group_id]
+        return temp_df[temp_df['species'] == self.master_species].iloc[0][fieldname]
+
+class Alignment:
+    def __init__(self, fileloc, master_species, alphabet_name, insert_symbol = '-'): #  group_id, mvave_len, remove_insertions = 'Y', consensus = 1):
+        temp = read_fasta_to_arrays(fileloc)
+        self.alphabet_name = alphabet_name
+        self.insert_symbol = insert_symbol
+        self.sequence_names = temp[0]
+        self.sequence_list = temp[1]
+        self.modified_sequence_list = temp[1]
+        self.num_sequences = len(self.sequence_names)
+        self.sequence_length = len(self.sequence_list[0])
+        self.modified_sequence_length = len(self.sequence_list[0])
+        self.master_species = master_species
+        self.master_species_index = self.sequence_names.index(self.master_species)  
+        self.relative_entropy = []
+        self.mvave_relative_entropy = [] 
+        self.master_species_modified_sequence_insertions = []
+        self.master_species_modified_sequence = self.modified_sequence_list[self.master_species_index]
+       
+    def modify_sequence(self, consensus):
+        self.modified_sequence_list = []
+        for j in range(self.num_sequences):
+            self.modified_sequence_list.append([])
+        for i in range(self.sequence_length):
+            temp = [x[i] for x in self.sequence_list]
+            if ((temp[self.master_species_index] == self.insert_symbol) and (temp.count(self.insert_symbol) >= consensus)):
+                pass
+            else:
+                for j in range(self.num_sequences):
+                    self.modified_sequence_list[j].append(temp[j])
+        for i in range(self.num_sequences):
+            self.modified_sequence_list[i] = ''.join(self.modified_sequence_list[i])
+        self.modified_sequence_length = len(self.modified_sequence_list[0])
+        self.master_species_modified_sequence = self.modified_sequence_list[self.master_species_index]
+        self.master_species_modified_sequence_insertions = []
+        other_sequences = []
+        for i in range(self.num_sequences):
+            if not ((i == self.master_species_index)):
+                other_sequences.append(self.modified_sequence_list[i])
+        for i in range(self.modified_sequence_length):
+            if not (self.master_species_modified_sequence[i] == self.insert_symbol):
+                sli = [x[i] for x in other_sequences]
+                if sli.count(self.insert_symbol) == self.num_sequences - 1:
+                    self.master_species_modified_sequence_insertions.append(i)
+        
+    def calculate_entropies(self, mvave_len = 1, modified=True):
+        if modified == True:
+            self.relative_entropy = relative_entropy(self.modified_sequence_list,alphabet_name = self.alphabet_name)
+        else:
+            self.relative_entropy = relative_entropy(self.sequence_list,alphabet_name = self.alphabet_name)
+        self.mave_relative_entropy = []
+        for k in range(len(self.relative_entropy)):
+            mv_temp = int(mvave_len/2)
+            if ((k + mv_temp <= len(self.relative_entropy)) and (k-mv_temp >= 0)):
+                self.mvave_relative_entropy.append(mean(self.relative_entropy[k-mv_temp:k+mv_temp]))
+            else:
+                self.mvave_relative_entropy.append(-np.inf)
+     
+    def alignment_position(self, pos):
+        if pos < 0:
+            temp_range = reversed(range(self.sequence_length))
+            pos = pos * - 1
+        else:
+            temp_range = range(self.sequence_length)
+        num_chars = 0
+        ans = -1
+        for i in temp_range:
+            temp =  self.sequence_list[self.master_species_index][i]
+            if not(temp == self.insert_symbol):
+                num_chars += 1
+            if num_chars == pos:
+                ans = i 
+                break
+        return ans   
