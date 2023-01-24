90fed006d761e28efa8df295d0df3c41558df373
nunder <90188497+nunder@users.noreply.github.com>
2023-01-24 12:18:11 +0000

    Changes 2023-01-24

--- Comparative_Analysis/.ipynb_checkpoints/Blast_Functions-checkpoint.py
+++ Comparative_Analysis/.ipynb_checkpoints/Blast_Functions-checkpoint.py
@@ -23,7 +23,7 @@ from . import Utilities as util
 
 def build_blast_db(seq_dir, seq_filename, blast_db_name, blast_dir, db_type = 'prot'):
     w_d = os.getcwd()
-    os.chdir("D:/")
+    os.chdir("F:/")
     subprocess.run('cd '+ seq_dir + ' &  makeblastdb -in ' + seq_filename + ' -dbtype '+ db_type + ' -out ' + blast_db_name, shell=True, capture_output = True)
     os.chdir(w_d)
     files_to_move = [x for x in util.list_files(seq_dir) if x[:-4] == blast_db_name]
@@ -34,24 +34,30 @@ def build_blast_db(seq_dir, seq_filename, blast_db_name, blast_dir, db_type = 'p
         
 def run_blastp(blast_dir, query_file, blast_db_name, e_value = 1e-10): 
     w_d = os.getcwd()
-    os.chdir("D:/")
+    os.chdir("F:/")
     subprocess.run('cd '+blast_dir+' & blastp -query '+blast_dir+'/'+query_file+' -db '+blast_db_name +' -out hits.csv -evalue '+str(e_value) +' -seg no -outfmt  "10 qaccver saccver qlen slen pident length mismatch gapopen qstart qend sstart send evalue bitscore" -num_threads 16', shell=True, capture_output = True)
     os.chdir(w_d)
     
 def run_blastn(blast_dir, query_file, blast_db_name, e_value = 1e-10): 
     w_d = os.getcwd()
-    os.chdir("D:/")
+    os.chdir("F:/")
     subprocess.run('cd '+blast_dir+' & blastn -query '+blast_dir+'/'+query_file+' -db '+blast_db_name +' -out hits.csv -evalue '+str(e_value) +' -outfmt  "10 qaccver saccver qlen slen pident length mismatch gapopen qstart qend sstart send evalue bitscore" -num_threads 16', shell=True, capture_output = True)
     os.chdir(w_d)
 
 def run_tblastx(blast_dir, query_file, blast_db_name, e_value = 1e-10): 
     w_d = os.getcwd()
-    os.chdir("D:/")
+    os.chdir("F:/")
     subprocess.run('cd '+blast_dir+' & tblastx -query '+blast_dir+'/'+query_file+' -db '+blast_db_name +' -out hits.csv -evalue '+str(e_value) +' -outfmt  "10 qaccver saccver qlen slen pident length mismatch gapopen qstart qend sstart send evalue bitscore" -num_threads 16', shell=True, capture_output = True)
+    os.chdir(w_d)
+    
+def run_tblastn(blast_dir, query_file, blast_db_name, e_value = 1e-10): 
+    w_d = os.getcwd()
+    os.chdir("F:/")
+    subprocess.run('cd '+blast_dir+' & tblastn -query '+blast_dir+'/'+query_file+' -db '+blast_db_name +' -out hits.csv -evalue '+str(e_value) +' -outfmt  "10 qaccver saccver qlen slen pident length mismatch gapopen qstart qend sstart send evalue bitscore" -num_threads 16', shell=True, capture_output = True)
     os.chdir(w_d)
 
 
-def process_blast_output(infile_loc, outfile_loc, names_dict, top_hit_only = False):
+def process_blast_output(infile_loc, outfile_loc, names_dict, top_hit_only = False, outfile_loc = None):
     blast_results = pd.read_csv(infile_loc, header = None)
     blast_results.columns = ['query_ref', 'target_ref', 'query_length', 'subject_length', 'percent_identical_matches','alignment_length', 'number_mismatches', 'number_of_gap_openings', 'query_start_alignment', 'query_end_alignment', 'target_start_alignment', 'target_end_alignment', 'e_value', 'bit_score']
     for i, r in blast_results.iterrows():
@@ -63,11 +69,12 @@ def process_blast_output(infile_loc, outfile_loc, names_dict, top_hit_only = Fal
     if top_hit_only == True:
         blast_results = blast_results.loc[blast_results.groupby(['query_ref','target_species'])['bit_score'].idxmax()]
     blast_results['species_count'] = blast_results.groupby('query_ref')['query_ref'].transform('size')
-    with open(outfile_loc, 'wb') as f:
-        pickle.dump(blast_results, f)
+    if not(outfile_loc == 'None'):
+        with open(outfile_loc, 'wb') as f:
+            pickle.dump(blast_results, f)
     return blast_results
 
-def keep_reciprocal_best_hits(query_df, reverse_query_df, outfile_loc):
+def keep_reciprocal_best_hits(query_df, reverse_query_df, outfile_loc ='None'):
     temp_1_dict = {}
     temp_2_dict = {}
     for i, r in query_df.iterrows():
@@ -80,8 +87,9 @@ def keep_reciprocal_best_hits(query_df, reverse_query_df, outfile_loc):
         else:
             query_df.at[i, 'reciprocal_best_hit'] = 'N'
     output = query_df[query_df.reciprocal_best_hit == 'Y'] 
-    with open(outfile_loc, 'wb') as f:
-        pickle.dump(output, f)
+    if not(outfile_loc == 'None'):
+        with open(outfile_loc, 'wb') as f:
+            pickle.dump(output, f)
     return output
 
 def align_keep_top_hit_per_species(files_dir, hit_file, alignment_file, output_alignment_file, package, evalue = 0.01): 
